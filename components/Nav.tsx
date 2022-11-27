c45548b8999ce2e2d742f76e8fe658f9043c9aa6
Elliot Adinolfi <elliot.adinolfi@gmail.com>
2022-11-26 22:27:39 -0500

    added dropdown

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,19 @@
 import styles from '../styles/Nav.module.css';
+import Image from 'next/image';
+import arrowDown from '../public/arrowDown.png'
+import { useState } from 'react';
 
 const Nav = () => {
+
+  const [ style, setStyle ] = useState({ opacity: "0"})
+
+  const clickedMenu = () => {
+    setStyle({ opacity: "1" })
+    setTimeout(() => {
+      setStyle({ opacity: "0" });
+    }, 2000);
+  };
+
   return (
     <div className={styles.navBar}>
       <div className={styles.leftNav}>
@@ -10,7 +23,22 @@ const Nav = () => {
         <span className={styles.logo}>{">"}</span>
       </div>
       <div className={styles.rightNav}>
-
+        <pre className={styles.type}>type </pre>
+        <pre className={styles.const}>questions = </pre>
+        <div>
+          <pre className={styles.menu} onClick={clickedMenu}>
+            Grind75 
+            <Image 
+              src={arrowDown}
+              alt="image of an arrow downward"
+              width={15}
+              height={15}
+            />
+          </pre>
+          <div className={styles.options} style={style}>
+            More Coming Soon! 😄
+          </div>
+        </div>
       </div>
     </div>
   )
