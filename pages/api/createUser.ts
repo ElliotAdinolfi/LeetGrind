45ab383a45dbb6e560be6f4cad4a7a7d1b1164fa
Elliot Adinolfi <elliot.adinolfi@gmail.com>
2022-12-03 20:24:04 -0500

    updated database

--- pages/api/createUser.ts
+++ pages/api/createUser.ts
@@ -8,9 +8,29 @@ const createUser = async (req: Request, res: Response) => {
     const db = client.db('LeetGrind');
     const { user } = req.body;
     const found = await db.collection('Users').findOne({ user: user });
+    
     if(!found) {
+      const qList = await db.collection('Questions').find({}).toArray();
+      const newUser: any = {
+        user: user,
+        topics: {},
+        totalReps: 0,
+        completeSections: 0
+      }
+      for(let i = 0; i < qList.length; i++) {
+        const topic = Object.keys(qList[i])[1];
+        const qArray = Object.keys(qList[i][topic]);
+        const initQVals: any = {
+          complete: 0
+        };
+        for(let j = 0; j < qArray.length; j++) {
+          const current = qArray[j];
+          initQVals[current] = 0;
+        };
+        newUser.topics[topic] = initQVals;
+      };
+      await db.collection('Users').insertOne(newUser);
       console.log('new user created')
-      await db.collection('Users').insertOne({ user: user });
     } else {
       console.log('found user')
     }
